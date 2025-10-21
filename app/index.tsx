5fe9dabda72eb0c5303c943c6d5502420d5e29c4
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-10-21 10:46:28 +0530

    Refactor index component to use FlatList for offers and add constants for images and categories

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,16 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { offers } from "@/constants"
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View className="flex-1 bg-white px-4">
-        <Text className="text-2xl font-bold text-primary font-quicksand-bold">
-          Welcome to React Native with Nativewind!
-        </Text>
-        <Text className="text-lg font-quicksand-light text-tertiary">It will be a food app with expo router</Text>
-      </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({item}) => (<View>
+          <Text>{item.title}</Text>
+        </View>)}
+      />
     </SafeAreaView>
-
   );
 }
