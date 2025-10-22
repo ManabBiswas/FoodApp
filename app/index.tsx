5e612a7ec1e38649daf521adbefac5a0e308c036
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-10-22 10:17:22 +0530

    Enhance UI components and structure by adding utility classes, updating image imports, and refining TypeScript configurations

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,34 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Pressable, Text, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { offers } from "@/constants"
+import { images, offers } from "@/constants"
+import { Fragment } from "react";
 
 export default function Index() {
+  const isEven = (num: number) => num % 2 === 0;
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <FlatList
+      <FlatList 
         data={offers}
-        renderItem={({item}) => (<View>
-          <Text>{item.title}</Text>
-        </View>)}
+        renderItem={({ item }) => (
+          <View>
+            <Pressable className={`offer-card ${isEven(item.id) ? "flex-row pr-10" : "flex-row-reverse pl-10"}`} style={{ backgroundColor: item.color }}>
+              {({ pressed }) => (
+                <Fragment>
+                  <View className="w-1/2 p-4 h-full justify-between" style={{ opacity: pressed ? 0.6 : 1 }}>
+                    <Image className={'size-full'} resizeMode={"contain"} source={item.image} />
+                  </View>
+                  <View className="offer-card__info ">
+                    <Text className="h1-bold text-white-100">{item.title}</Text>
+                    <Image source={images.arrowRight} className="size-10 " resizeMode="contain" />
+                  </View>
+                </Fragment>
+              )}
+            </Pressable>
+          </View>
+        )}
+        contentContainerClassName="py-14 px-2"
       />
     </SafeAreaView>
   );
 }
+
