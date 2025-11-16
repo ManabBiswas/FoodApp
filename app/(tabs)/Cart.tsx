0643d1810017c97d5c6a391ed88f0935ba4b088d
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-11-16 19:47:24 +0530

    Add image to Cart component

--- app/(tabs)/Cart.tsx
+++ app/(tabs)/Cart.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { images } from '@/constants';
+import { Image } from 'react-native';
 
 const Cart = () => {
   return (
     <View>
+      <Image source={images.bag} />
       <Text>Cart</Text>
     </View>
   )
